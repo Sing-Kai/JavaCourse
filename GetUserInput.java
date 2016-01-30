3c9214382cad86b955b6cfc0d1f374f37e5cf442
Sing-Kai <singkai.chiu@gmail.com>
2016-01-30 22:37:15 +0000

    Scans for user input

---
+++ GetUserInput.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+class GetUserInput{
+	public static void main(String argv[]){
+		Scanner userInput = new Scanner(System.in);
+		System.out.println(userInput.nextLine());
+	}
+}
